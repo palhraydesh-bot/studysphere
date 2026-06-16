7c983c604ef434e049be058e574c442e9e08ef05
Shubham <palhraydesh@gmail.com>
2026-06-16 18:42:45 +0530

    fix CORS popup

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,19 @@ const nextConfig = {
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' }
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
