d9d3150aa03abad8dc8c2ba0360dae80e5590585
Shubham <palhraydesh@gmail.com>
2026-06-21 22:02:21 +0530

    journal fixed

---
+++ src/app/(dashboard)/dashboard/growth-os/page.tsx
@@ -0,0 +1,12 @@
+export default function GrowthOSPage() {
+  return (
+    <div style={{
+      background: "red",
+      color: "white",
+      fontSize: "40px",
+      padding: "50px"
+    }}>
+      GROWTH OS PAGE
+    </div>
+  );
+}
